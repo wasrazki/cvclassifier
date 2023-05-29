d07df154967ebee140ae9f55fd67e586875b1a00
KilaniMaryem <mariem.kilani@insat.ucar.tn>
2023-05-29 15:48:13 +0100

    delete some comments

--- src/app/app/upload-cv/upload-cv.component.ts
+++ src/app/app/upload-cv/upload-cv.component.ts
@@ -15,7 +15,7 @@ export class UploadCvComponent implements OnInit {
   }
   /*********/
   onFileSelected(event: Event): void {
-    console.log("onFileSelected begins executing");
+    
     const fileInput = event.target as HTMLInputElement;
     const files = fileInput.files;
     // Handle the selected files here
@@ -23,17 +23,17 @@ export class UploadCvComponent implements OnInit {
        this.selectedFile = files[0];
       // Perform any necessary operations with the file
       console.log('Selected file:', this.selectedFile);
-      console.log("onFileSelected is done executing");
+      
     }}
   /*********/
   onSubmit(): void {
-    console.log("onSubmit begins executing");
+    
     if (this.selectedFile) {
-      console.log("9bal l FormData = new FormData() bedhabt");
+      
       const formData: FormData = new FormData();
-      console.log("9bal l formData.append('cv', this.selectedFile); bedhabt");
+      
       formData.append('cv', this.selectedFile);
-      console.log("9bal l flaskapiService.uploadCv bedhabt");
+      
       this.flaskApiService.uploadCv(formData).subscribe({
         next: (response: any) => {
           console.log('File uploaded successfully!');
@@ -44,7 +44,7 @@ export class UploadCvComponent implements OnInit {
         }
       });
     }
-    console.log("onSubmit is done executing");
+    
   }
   
   
